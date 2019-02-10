47008a8bcd6f9631b8e4cb2fcd64c78a2467b748
Jordi Guillaumes Pons <jguillaumes@gmail.com>
2019-02-10 23:00:53 +0100

    Initial Commit

---
+++ DecnetCalc/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  DecnetCalc
+//
+//  Created by Jordi Guillaumes Pons on 10/02/2019.
+//  Copyright © 2019 Jordi Guillaumes Pons. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
