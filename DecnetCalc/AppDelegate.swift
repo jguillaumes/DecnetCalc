15689bd70226cdb3f4701f879deac57fb3ae4f61
Jordi Guillaumes Pons <jguillaumes@gmail.com>
2019-02-11 00:12:08 +0100

    Primera revisió

--- DecnetCalc/AppDelegate.swift
+++ DecnetCalc/AppDelegate.swift
@@ -13,15 +13,36 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
     @IBOutlet weak var window: NSWindow!
 
-
+    @IBOutlet weak var dnArea: NSTextField!
+    @IBOutlet weak var dnNode: NSTextField!
+    @IBOutlet weak var macAddress: NSTextField!
+    
+    @IBAction func computeMac(_ sender: NSTextField) {
+        
+        if sender == dnArea || sender == dnNode {
+        
+            let nArea = dnArea.integerValue
+            let nNode = dnNode.integerValue
+            let dnAddress = 1024 * nArea + nNode
+            let hexAddress = String(dnAddress, radix:16, uppercase: true)
+            let byteOne = String(hexAddress.prefix(2))
+            let byteTwo = String(hexAddress.suffix(2))
+        
+            macAddress.stringValue = "0A:00:04:00:\(byteTwo):\(byteOne)"
+        }
+    }
+    
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+        
+        dnArea.stringValue = "7"
+        dnNode.stringValue = "61"
+        computeMac(dnNode)
+        
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
-
 
 }
 
